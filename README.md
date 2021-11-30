7d0985e7bee56731566bc48bff203124df3b54f5
cat <nobody@email.com>
2021-11-29 23:18:52 -0600

    Initial skeleton project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kafka Playground
+Based on Pluralsight course "**Designing Event-driven Applications Using Apache Kafka Ecosystem**"
+                            by Bogdan Sucaciu
---
+++ user-tracking-consumer/src/main/java/com.pluralsight.user.tracking.consumer/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight.user.tracking.consumer;
+
+
+import com.pluralsight.kafka.model.User;
+import org.apache.avro.LogicalTypes;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
